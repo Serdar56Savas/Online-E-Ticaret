3c63561d6b4b560815ddd636983d9a9cb6f2e1d7
Serdar Savaş <77737076+SerdarOytun@users.noreply.github.com>
2021-12-21 00:26:39 +0300

    web
    20/12/2021

---
+++ OnlineShopping/OnlineShopping/Controllers/CariPanelController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineShopping.Models.Sınıflar;
+
+namespace OnlineShopping.Controllers
+{
+    public class CariPanelController : Controller
+    {
+        // GET: CariPanel
+        Context c = new Context();
+        [Authorize]
+        public ActionResult Index()
+        {
+            var mail = (string)Session["cariMail"];
+            var degerler = c.Carilers.FirstOrDefault(x => x.cariMail == mail);
+            ViewBag.m = mail;
+            return View(degerler);
+        }
+        public ActionResult Siparislerim()
+        {
+            var mail = (string)Session["cariMail"];
+            var id = c.Carilers.Where(x => x.cariMail == mail.ToString()).Select(y => y.cariID).FirstOrDefault();
+            var degerler = c.SatisHarekets.Where(x => x.cariid == id).ToList();
+            return View(degerler);
+        }
+    }
+}
